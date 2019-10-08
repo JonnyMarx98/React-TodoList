7fcb36e9af7cf3948778e994e1e918b0dda0b062
Jonny <jonnymarx98@gmail.com>
2019-10-08 07:45:32 +0100

    Updates

---
+++ precache-manifest.27e0b83d66d90b21e5b206c77d91be69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938428ff4f06e2a56173722b796bcc8e",
+    "url": "/JonnyMarx98/React-TodoList/index.html"
+  },
+  {
+    "revision": "ebffc21650bc128b88b5",
+    "url": "/JonnyMarx98/React-TodoList/static/css/main.a1214ac9.chunk.css"
+  },
+  {
+    "revision": "2f057cc455ae2c1bfcba",
+    "url": "/JonnyMarx98/React-TodoList/static/js/2.f7d62112.chunk.js"
+  },
+  {
+    "revision": "ebffc21650bc128b88b5",
+    "url": "/JonnyMarx98/React-TodoList/static/js/main.8ab087b1.chunk.js"
+  },
+  {
+    "revision": "2b19423a9c94ec8e215b",
+    "url": "/JonnyMarx98/React-TodoList/static/js/runtime-main.bf2c56d5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/JonnyMarx98/React-TodoList/static/media/logo.25bf045c.svg"
+  }
+]);
